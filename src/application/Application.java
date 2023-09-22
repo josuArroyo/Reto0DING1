dd7f8964469193ede2e38b91ef46140a9547b26d
EgoitzyJosu <2dam@HZ375750.TartangaLH.eus>
2023-09-22 10:54:38 +0200

    cambio de la clase application

--- src/application/Application.java
+++ src/application/Application.java
@@ -8,7 +8,7 @@ package application;
 import controller.Controller;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.application.Application;
+//import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -20,7 +20,7 @@ import view.ViewFactory;
  *
  * @author 2dam
  */
-public class HelloWorldGreating extends Application {
+public class Application extends javafx.application.Application {
 
     @Override
     public void start(Stage stage) throws Exception {
